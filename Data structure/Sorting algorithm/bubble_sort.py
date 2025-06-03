191770b038a4becf92b68d905e6126ca62739a6d
Devarsh005 <devarsh.rathod.reversebits@gmail.com>
2025-06-03 18:36:21 +0530

    UPDATE - restructure the code into the function based

--- Data structure/Sorting algorithm/bubble_sort.py
+++ Data structure/Sorting algorithm/bubble_sort.py
@@ -1,8 +1,12 @@
-numbers = [7,19,-3,4,0]
-n = len(numbers)
+def bubble_sort(array):
+    """bubble sort algorithm"""
+    n = len(array)
 
-for i in range(1,n):
-    for j in range(0,n-i):
-        if numbers[j] > numbers[j+1]:
-            numbers[j] , numbers[j+1] = numbers[j+1] , numbers[j]
-print(numbers)
+    for i in range(1,n):
+        for j in range(0,n-i):
+            if array[j] > array[j+1]:
+                array[j] , array[j+1] = array[j+1] , array[j]
+    print(array)
+
+numbers = [7,19,-3,4,0]
+bubble_sort(numbers)
