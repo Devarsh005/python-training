36194713f38a89d78e7223c807692ab7bc1778bf
Devarsh005 <devarsh.rathod.reversebits@gmail.com>
2025-06-03 17:57:04 +0530

    ADD NEW FILE - selection-sort file added

---
+++ Data structure/Sorting algorithm/selection_sort.py
@@ -0,0 +1,17 @@
+# why selection sort made
+# what is the complexity of selection sort
+# how selection sort work
+# is there any efficient way to do 
+
+data = [25,15,-9,0,8,6]
+
+def selection_sort(data):
+    size = len(data)
+    for i in range(size):
+        minimum = i
+        for next in range(i+1,size):
+            if data[next] < data[minimum] :
+                minimum = next
+        data[i] ,data[minimum] = data[minimum] , data[i]
+    print(data)
+selection_sort(data)
