224523a9d91fd497703e9f827274e180b09faa68
Devarsh005 <devarsh.rathod.reversebits@gmail.com>
2025-06-05 16:06:18 +0530

    Added -linear_search.py

---
+++ Data structure/Searching algorithm/linear_search.py
@@ -0,0 +1,9 @@
+numbers = [-12, 5, 11, 22, 25, 34, 64, 90]
+search = 0
+def linear_search(arr):
+    for index,element in enumerate(arr):
+        if element == search:
+            return f"element at index {index}"
+    else:
+        return f"not found"
+print(linear_search(numbers))
