356855cfd1aec0a677229b1891b03b4b5bbf6431
Devarsh005 <devarsh.rathod.reversebits@gmail.com>
2025-06-11 19:17:40 +0530

    update add a loan and pay emi section in match case 13 and 14 and create a function in Account class

--- Data structure/bank/bank.py
+++ Data structure/bank/bank.py
@@ -3,7 +3,8 @@ class Bank:
     bank_name = "HDFC bank"
     def __init__(self):
         self.list_of_accounts = {}
-        self.balance = 1_00_00_000
+        self.balance = 1_00_000
+        self.loan_users = []
     # create a new account
     def create_account(self,name:str,password:str,account_number:str):
         """create Account if it is not exits"""
@@ -13,13 +14,20 @@ class Bank:
             account = Account(name,password,account_number)
             self.list_of_accounts[account_number] = account
             return True
-    def authentication(self,account_number:str,password:str):
+    def authentication(self,account_number:str,password:str)->'Account':
         """return the account if the credential is right"""
         account = self.list_of_accounts.get(account_number)
         if account and account.check_password(password):
             return account
         else:
             raise validations.AccountNotFoundError("AccountNotFoundError: this account not registered in bank")
+    def bank_info(self):
+        """shows the bank name , bank balance , bank account list"""
+        print("HDFC BANK")
+        print(f"Bank balanace = {self.balance}")
+        print(f"accounts list {self.list_of_accounts.keys()}")
+    
+    
     # open particular account
     # 
 
@@ -67,17 +75,29 @@ class Account:
         else:
             raise validations.InsuffiecientFundError("InsuffiecientFundError: insuffiecient balance")
     def transaction(self):
-        """show the transaction of user"""
+        """show the transaction history of user"""
         for transaction in self.transactions:
             print(transaction)
     def check_balance(self):
         """return the balance of user """
         return self.balance
+    def take_a_loan(self,amount,duration):
+        interest_rate = 7
+        total_with_interest = amount + (amount*interest_rate/100)
+        emi = total_with_interest/duration
+        self.balance += amount
+        self.info['balance'] = self.balance
+        print("loan passed")
+        print(f"your minimum due per month is {emi}")
+        return total_with_interest,emi
+    def pay_emi(self , due_amount,remaining_amount,duration):
+        remaining_amount -= due_amount
+        duration -= 1
+        self.balance -= due_amount
+        self.info["balance"] = self.balance
+        return remaining_amount,duration
 
 
-# create a user dictionary to store accounts only
-# import json
-# users = open("users.json","w")
 if __name__ == "__main__":
     bank = Bank()
 
@@ -93,6 +113,9 @@ if __name__ == "__main__":
     9. show transaction
     10. show menu script
     11. show all users
+    12. show Bank profile
+    13. take a loan
+    14. pay emi
                 """
     print(cases)
     flag = True
@@ -145,6 +168,7 @@ if __name__ == "__main__":
                     amount = float(input("enter amount - "))
                     account.add_money(amount)
                     print(f"{amount} added . current balance - {account.check_balance()}")
+                    bank.balance += amount
                 except Exception as e:
                     print(e)
 
@@ -157,6 +181,7 @@ if __name__ == "__main__":
                     amount = float(input("enter amount - "))
                     account.withdraw_money(amount)
                     print(f"{amount} withdraw . current balance - {account.check_balance()}")
+                    bank.balance -= amount
                 except Exception as e:
                     print(e)
 
@@ -164,6 +189,9 @@ if __name__ == "__main__":
                 """close account"""
                 try:
                     account_number = input("enter the account number - ")
+                    password = input("enter tha password")
+                    account = bank.authentication(account_number,password)
+                    bank.balance -= account.balance
                     del bank.list_of_accounts[account_number]
                     print(f"close the account")
                 except Exception as e:
@@ -211,6 +239,45 @@ if __name__ == "__main__":
             case "11":
                 """show all users"""
                 for account_number,account in bank.list_of_accounts.items():
-                    print(f"{account_number} : {account.profile()}")
+                    print(f"{account_number} : {account.info}")
+            case "12":
+                """show bank info"""
+                bank.bank_info()
+            case "13":
+                """take a loan from bank"""
+                try:
+                    account_number = input("enter the account number - ")
+                    password = input("enter the password - ")
+                    account = bank.authentication(account_number,password)
+                    amount = int(input("enter amount of loan"))
+                    if amount < (bank.balance*20/100) :
+                        bank.loan_users.append(account.account_number)
+                        duration = int(input("enter duration for loan"))
+                        total_amount_with_interest , due_amount = account.take_a_loan(amount,duration)
+                        print(f"{account.name} account balance is {account.balance}")
+                        bank.balance -= amount
+                    else:
+                        print(f"amount is larget , amount should be less than {bank.balance*20/100}")
+                except Exception as e:
+                    print(e)
+            case "14":
+                """pay emi"""
+                try:
+                    account_number = input("enter the account number - ")
+                    password = input("enter the password - ")
+                    account = bank.authentication(account_number,password)
+                    if account_number in bank.loan_users:
+                        if duration != 0:
+                            total_amount_with_interest , duration = account.pay_emi(due_amount,total_amount_with_interest,duration)
+                            bank.balance += due_amount
+                            print(f"{due_amount} is paid and user current balance is {account.balance}")
+                            print(f"{duration} are remaining")
+                        else:
+                            print("all dues are paid ")
+                            break
+                except Exception as e:
+                    print(e)
+                    
+
             case _:
                 flag = False
