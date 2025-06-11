54f9c1f35d9ad345fb9976ccf9a6115a9492fa9f
Devarsh005 <devarsh.rathod.reversebits@gmail.com>
2025-06-11 16:28:26 +0530

    update added a account_number and password validations and also created a custom exceptions for some requirements

--- Data structure/bank/validations.py
+++ Data structure/bank/validations.py
@@ -1,24 +1,73 @@
+# import json for check the account is duplicate or not
+import json
+
+# create a custom Exception
+class DuplicateAccountError(Exception):
+    ...
+class InsuffiecientFundError(Exception):
+    ...
+class AccountNotFoundError(Exception):
+    ...
 def is_valid_name(name:str):
+    """raise Error if the name is invalid"""
     if name:
         for character in name:
             if character.isdigit():
                 raise TypeError("TypeError: name cannot be a int , float , bool or None")
         if name.lower() in ["true","false","none"]:
             raise ValueError("ValueError: value could not true , false and none")
-        else:
-            return True
     else:
         raise ValueError("name cannot be an empty")
-# def is_valid_account(account_number):
-#     if isinstance(account_number,int):
-#         return account_number
-#     else:
-#         raise Exception("account should be an integer")
-# def is_strong_password(account_number):
-#     ...
-# try:
-#     name = input("enter a name")
-#     x =is_valid_name(name)
-#     print(x)
-# except Exception as e:
-#     print(e)
+    
+def is_valid_account(account_number:str):
+    """Raise Error if the account number is not valid"""
+    if account_number:
+        for digit in account_number:
+            if  not digit.isdigit():
+                raise TypeError("TypeError: Account number must numeric")
+        if not int(account_number)>0:
+            raise ValueError("valueError: account number should be greater than zero")
+    else:
+        raise ValueError("ValueError: account number cannot empty")
+
+def is_valid_password(password:str):
+    """check the password is valid or not"""
+    if password:
+        size = len(password)
+        special_character = ["@","$","!","#","%","^","&","*","(",")",",","."]
+        def must_one_special_character(password,special_character):
+            """return true if the password must have special character otherwise return false"""
+            for char in password:
+                if char in special_character:
+                    return True
+            else:
+                return False
+        def any_uppercase(password:str):
+            """return True if any uppercase chracter in password"""
+            for char in password:
+                if char.isupper():
+                    return True
+            else:
+                return False
+        def any_lowercase(password:str):
+            """return True if any lowercase chracter in password"""
+            for char in password:
+                if char.islower():
+                    return True
+            else:
+                return False
+        if not size>=8 :
+            raise ValueError("ValueError: password length must be greater than 8")
+        elif not must_one_special_character(password,special_character):
+            raise ValueError("ValueError: password must have one special character")
+        elif not any_uppercase(password):
+            raise ValueError("ValueError: password must have one uppercase character")
+        elif not any_lowercase(password):
+            raise ValueError("ValueError: password must have one lowercase character")
+    else:
+        raise ValueError("ValueError: password cannot be empty")
+    
+def is_account_duplicate(account_number:str):
+    with open("users.json","r") as file:
+        acc_number = json.load(file)
+        print(acc_number)
